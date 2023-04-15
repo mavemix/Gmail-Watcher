84fee9a56f23b5a68aea241b83b97d4ccfc7334f
Andrés <andres.alvarez.alvarez@gmail.com>
2023-04-15 02:52:04 +0200

    Conexión a Gmail, obtención de listado de mensajes

---
+++ src/index.php
@@ -0,0 +1,84 @@
+<?php 
+
+namespace Andres\Telefonica;
+
+require_once '../vendor/autoload.php';
+
+session_start();
+
+$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+$oauth_credentials = '../credentials.json';
+
+$client = new \Google\Client();
+$client->setAuthConfig($oauth_credentials);
+$client->setRedirectUri($redirect_uri);
+$client->setScopes('https://www.googleapis.com/auth/gmail.readonly');
+$client->setAccessType('offline');
+
+if (isset($_GET['logout'])) {
+    unset($_SESSION['id_token_token']);
+}
+
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+
+  // store in the session also
+  $_SESSION['id_token_token'] = $token;
+  // redirect back to the example
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  return;
+}
+  
+  /************************************************
+   If we have an access token, we can make
+   requests, else we generate an authentication URL.
+   ************************************************/
+if (!empty($_SESSION['id_token_token']) && isset($_SESSION['id_token_token']['access_token'])) {
+    $client->setAccessToken($_SESSION['id_token_token']);
+} else {
+  $authUrl = $client->createAuthUrl();
+}
+/************************************************
+  If we're signed in we can go ahead and retrieve
+  the ID token, which is part of the bundle of
+  data that is exchange in the authenticate step
+  - we only need to do a network call if we have
+  to retrieve the Google certificate to verify it,
+  and that can be cached.
+************************************************/
+if ($client->isAccessTokenExpired()) {
+  $_SESSION['id_token_token'] = $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+}
+
+if(isset($authUrl)) {
+  echo "
+  <div class='request'>
+    <a class='login' href='".$authUrl."'>Conectar cuenta de Google</a>
+  </div>";
+
+} else {
+  echo "Logueado!<br><br>";
+}
+  
+
+$service = new \Google\Service\Gmail($client);
+
+try{
+
+  // Get the especific messages
+  $user = 'me';
+  $results = $service->users_messages->listUsersMessages($user,[]);
+
+  foreach($results->messages as $message) {
+    echo "ID: ".$message->id."<br>Thread: ".$message->threadId."<hr>";
+  }
+  
+}
+catch(\Exception $e) {
+    // TODO(developer) - handle error appropriately
+    echo 'Message: ' .$e->getMessage();
+}
+
+  ?>
+
+
